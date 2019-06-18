cbd8fe7127a9f4676e15f82ec8c1b3016d2b23e9
ancymicheal <ncysus17@gmail.com>
2019-06-18 10:36:23 +0530

    Update floatim.py

--- floatim.py
+++ floatim.py
@@ -4,6 +4,8 @@ from kivy.uix.floatlayout import FloatLayout
 
 class simple(App):
     def build(self):
+        Config.set('graphics', 'resizable', '0')
+        Config.set('graphics', 'width', '900')
         return FloatLayout()
 
 if __name__ == "__main__":
