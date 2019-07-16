4cb7a1b0e96e7fb073be7e697309165431858f74
ancymicheal <ncysus17@gmail.com>
2019-07-16 17:10:46 +0530

    Update ROLO_utils.py

--- ROLO/utils/ROLO_utils.py
+++ ROLO/utils/ROLO_utils.py
@@ -1,27 +1,4 @@
-# Copyright (c) <2016> <GUANGHAN NING>. All Rights Reserved.
 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#    http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License. 
-
-'''
-Script File: ROLO_utils.py
-
-Description:
-      
-	ROLO is short for Recurrent YOLO, aimed at simultaneous object detection and tracking
-	Paper: http://arxiv.org/abs/1607.05781
-	Author: Guanghan Ning
-	Webpage: http://guanghan.info/
-'''
 import os, sys, time
 import numpy as np
 import ConfigParser
